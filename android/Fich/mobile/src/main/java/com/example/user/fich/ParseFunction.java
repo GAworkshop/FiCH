37f13af2f82eb551a473467562d753ecc6675ca9
huang831206 <huang831206@gmail.com>
2016-08-05 23:47:38 +0800

    20160805_UI_branch_init

---
+++ android/Fich/mobile/src/main/java/com/example/user/fich/ParseFunction.java
@@ -0,0 +1,10 @@
+package com.example.user.fich;
+
+import org.json.JSONArray;
+
+/**
+ * Created by huang on 2016/8/4.
+ */
+public interface ParseFunction{
+    public abstract JSONArray parse(String result);
+}
---
+++ android/Fich/mobile/src/main/java/com/example/user/fich/SearchableActivity.java
@@ -0,0 +1,27 @@
+package com.example.user.fich;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class SearchableActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handleIntent(getIntent());
+    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+        }
+    }
+}
