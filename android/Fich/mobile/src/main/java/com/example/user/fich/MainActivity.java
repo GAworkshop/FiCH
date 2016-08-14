44e161ee914e292c4c8ebe664bd80019fb8770a7
huang831206 <huang831206@gmail.com>
2016-08-15 00:19:25 +0800

    20160815_tabs

--- android/Fich/mobile/src/main/java/com/example/user/fich/MainActivity.java
+++ android/Fich/mobile/src/main/java/com/example/user/fich/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -13,21 +14,24 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ListView;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import layout.CardFragment;
 import layout.ContactFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
 
-    private ListView left_drawer;
+    private NavigationView left_drawer;
 
     private ActionBarDrawerToggle mActionBarDrawerToggle;
 
@@ -37,9 +41,15 @@ public class MainActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
 
-    private String[] tab_titles = {"醫療資訊卡", "緊急聯絡人", "個人資訊", "鄰近醫療單位", "逃生指引"};
-    private int[] fragmants_list = {R.layout.fragment_contact, R.layout.fragment_card,
-            R.layout.fragment_card, R.layout.fragment_card, R.layout.fragment_card};
+    private String[] tab_titles = {"資訊卡", "緊急聯絡人", "個人資訊", "鄰近醫療單位", "逃生指引"};
+    private int[] tab_icons = {
+            R.drawable.ic_card,
+            R.drawable.ic_contact,
+            R.drawable.ic_info,
+            R.drawable.ic_nearby,
+            R.drawable.ic_escape
+    };
+    //private int[] fragmants_list = {R.layout.fragment_contact, R.layout.fragment_card, R.layout.fragment_card, R.layout.fragment_card, R.layout.fragment_card};
 
     SensorManager mSensorMgr;
     TextView tx, ty, tz;
@@ -76,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
 */
 
         //startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        //startActivity(new Intent(MainActivity.this, SettingsActivity.class));
 
         tx = (TextView) findViewById(R.id.textView6);
         ty = (TextView) findViewById(R.id.textView7);
@@ -84,6 +95,7 @@ public class MainActivity extends AppCompatActivity {
 
         initToolbar();
         initViewPager();
+        setListeners();
 
 /*
         mSensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
@@ -108,6 +120,15 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (this.mDrawerLayout.isDrawerOpen(left_drawer)) {
+            this.mDrawerLayout.closeDrawer(left_drawer);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -136,7 +157,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        left_drawer = (ListView) findViewById(R.id.left_drawer);
+        left_drawer = (NavigationView) findViewById(R.id.left_drawer);
 
         mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close) {
             @Override
@@ -163,7 +184,23 @@ public class MainActivity extends AppCompatActivity {
         //mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabs));
 
         mTabs.setupWithViewPager(mViewPager);
+        //mTabs.getTabAt(0).setIcon(R.drawable.ic_card);
 
+        //mTabs.getTabAt(0).setCustomView();
+
+        for (int i = 0; i < tab_icons.length; i++){
+
+
+
+            View v = LayoutInflater.from(this).inflate(R.layout.tab_item, null);
+            ((ImageView)v.findViewById(R.id.iv_tab_icon)).setImageResource(tab_icons[i]);
+            ((TextView)v.findViewById(R.id.tv_tab_icon)).setText(tab_titles[i]);
+            mTabs.getTabAt(i).setCustomView(v);
+
+
+
+            //mTabs.getTabAt(i).setIcon(tab_icons[i]);
+        }
 /*
         mTabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
@@ -201,6 +238,19 @@ public class MainActivity extends AppCompatActivity {
 */
     }
 
+    private void setListeners(){
+        left_drawer.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                Log.e("~~DEBUG~~", "" + item.getOrder());
+                item.setChecked(true);
+                mViewPager.setCurrentItem(item.getOrder());
+                mDrawerLayout.closeDrawer(left_drawer);
+                return false;
+            }
+        });
+    }
+
     private class SamplePagerAdapter extends FragmentPagerAdapter {
 
         public SamplePagerAdapter(android.support.v4.app.FragmentManager fm){
@@ -214,14 +264,30 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public android.support.v4.app.Fragment getItem(int position){
+
+            switch (position){
+                case 0:
+                    return CardFragment.newInstance("", "");
+                case 1:
+                    return ContactFragment.newInstance("","");
+                case 2:
+                case 3:
+                case 4:
+                default:
+                    return new Fragment();
+            }
+
+            /*
             if(position == 0)
                 return ContactFragment.newInstance("","");
             else
                 return new Fragment();
+            */
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
+            //return null;
             return tab_titles[position];
         }
 
@@ -248,6 +314,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Log.e("~~DEBUG~~", "onOptionsItemSelected: " + item.getItemId());
         switch (item.getItemId()) {
             case R.id.action_about:
                 // About option clicked.
